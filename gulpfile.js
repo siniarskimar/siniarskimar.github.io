ee408bb48d1f035daecb3db035495366f871b1c3
Marcin Siniarski <siniarskimar21@gmail.com>
2020-06-06 16:22:39 +0200

    Watch for sass changes in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,4 +10,9 @@ function compileSass() {
         .pipe(gulp.dest("./css"));
 }
 
+function watch() {
+    gulp.watch("./src/scss/*.scss", compileSass);
+}
+
 exports.compileSass = compileSass;
+exports.watch = watch;
